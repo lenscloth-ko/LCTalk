2e5b2a7f46284fe708bad599604fdcedbf686bff
이정규 <i6020345@live.co.kr>
2018-12-21 05:55:45 +0900

    대화부분 추가

---
+++ LCTalk.Web/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using LCTalk.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LCTalk.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public ActionResult Login()
+        {
+            string user_name = Request.Form["username"];
+
+            if (user_name.Trim() == "")
+            {
+                return Redirect("/");
+            }
+
+            using (var db = new Models.ChatContext())
+            {
+
+                User user = db.Users.FirstOrDefault(u => u.name == user_name);
+
+                if (user == null)
+                {
+                    user = new User { name = user_name };
+
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+
+                Session["user"] = user;
+            }
+
+            return Redirect("/chat");
+        }
+    }
+}
--- LCTalk.Web/Models/ChatContext.cs
+++ LCTalk.Web/Models/ChatContext.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace LCTalk.Web.Models
 {
-    public class ChatContext
+    public class ChatContext : DbContext
     {
+        public ChatContext() : base("DefaultConnection")
+        {
+        }
 
+        public static ChatContext Create()
+        {
+            return new ChatContext();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Conversation> Conversations { get; set; }
     }
 }
