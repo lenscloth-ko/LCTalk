0c80ce22b0c6cee97e148dac945140be732cddc8
이정규 <i6020345@live.co.kr>
2018-12-20 11:42:44 +0900

    route 설정 user class 추가

--- LCTalk.Web/Controllers/HomeController.cs
+++ LCTalk.Web/Controllers/HomeController.cs
@@ -10,6 +10,11 @@ namespace LCTalk.Web.Controllers
     {
         public ActionResult Index()
         {
+            if (Session["user"] != null)
+            {
+                return Redirect("/chat");
+            }
+
             return View();
         }
 
---
+++ LCTalk.Web/Models/ChatContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LCTalk.Web.Models
+{
+    public class ChatContext
+    {
+
+    }
+}
